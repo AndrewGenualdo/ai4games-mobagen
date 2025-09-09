56b5690c44980d046b4e3083217bac0bf31dcaa9
AndrewGenualdo <andrew.genualdo@mymail.champlain.edu>
2025-09-09 14:12:53 -0400

    game of life works

--- examples/life/rules/JohnConway.cpp
+++ examples/life/rules/JohnConway.cpp
@@ -1,11 +1,28 @@
 #include "JohnConway.h"
 
+#include <iostream>
+#include <bits/ostream.tcc>
+
 // Reference: https://playgameoflife.com/info
 void JohnConway::Step(World& world) {
-  // todo: implement
+  for (int y = 0; y < world.SideSize(); y++) {
+    for (int x = 0; x < world.SideSize(); x++) {
+
+      int const neighbors = CountNeighbors(world, Point2D(x, y));
+      const bool wasAlive = world.Get(Point2D(x, y));
+      const bool alive = wasAlive ? (neighbors == 2 || neighbors == 3) : neighbors == 3;
+      world.SetNext(Point2D(x, y), alive);
+    }
+  }
 }
 
 int JohnConway::CountNeighbors(World& world, Point2D point) {
-  // todo: implement
-  return 0;
+  int count = 0;
+  for (int y = -1; y <= 1; y++) {
+    for (int x = -1; x <= 1; x++) {
+      if (x == 0 && y == 0) continue;
+      count += world.Get(Point2D(point.x + x, point.y + y)) ? 1 : 0;
+    }
+  }
+  return count;
 }
